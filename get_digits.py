68d5eb6ac568f26a28736714b696273cef49f82c
Tim van Werkhoven <tvwerkhoven@github>
2020-03-22 20:14:50 +0100

    Add logging level

--- get_digits.py
+++ get_digits.py
@@ -722,10 +722,14 @@ def main():
 
     # Pre-process command-line arguments
     args = parser.parse_args()
-    logging.basicConfig(filename=args.logfile, level=logging.INFO, format='%(asctime)s %(message)s')
+    logging.basicConfig(filename=args.logfile, level=logging.INFO, format='%(asctime)s %(levelname)-8s %(message)s')
     # define a Handler which writes INFO messages or higher to the sys.stderr
     console = logging.StreamHandler()
     console.setLevel(logging.DEBUG)
+    # set a format which is simpler for console use
+    formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
+    # tell the handler to use this format
+    console.setFormatter(formatter)
     # add the handler to the root logger
     logging.getLogger('').addHandler(console)
 
