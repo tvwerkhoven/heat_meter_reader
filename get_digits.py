f342dc02f392eef86ca2c3e22dc018b7bb9a4c94
Tim van Werkhoven <tvwerkhoven@github>
2020-03-22 20:13:11 +0100

    Add console logger for debug

--- get_digits.py
+++ get_digits.py
@@ -533,7 +533,7 @@ def calc_value(digit_levels, segthresh, minval=0, maxval=0):
             return cand, prob
 
         # Candidate was not ok, update next most probable digit and retry
-        logging.warning("Skipping NOK candidate value {} with distance {:.4}...".format(cand, prob))
+        logging.warning("Skipping candidate value {} with distance {:.4}...".format(cand, prob))
         cand_id[cand_id_next[0][off+ndigit]] = cand_id_next[1][off+ndigit]
     
     # If we exit the loop above we found no match that satisfies numerical 
@@ -723,6 +723,11 @@ def main():
     # Pre-process command-line arguments
     args = parser.parse_args()
     logging.basicConfig(filename=args.logfile, level=logging.INFO, format='%(asctime)s %(message)s')
+    # define a Handler which writes INFO messages or higher to the sys.stderr
+    console = logging.StreamHandler()
+    console.setLevel(logging.DEBUG)
+    # add the handler to the root logger
+    logging.getLogger('').addHandler(console)
 
     if (args.debug):
         print (args)
